1ce2c07b7e6a4fe02217da853be4cb3cf4e5185d
DevDoge <lhuthefranc@hotmail.com>
2021-09-29 10:13:53 -0300

    Initial commit

---
+++ src/main/java/br/com/univesp/IntegradorProjeto/IntegradorProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.univesp.IntegradorProjeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradorProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/univesp/IntegradorProjeto/IntegradorProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.univesp.IntegradorProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
